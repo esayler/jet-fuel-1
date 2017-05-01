6c116ab9bef7a9fce95db2129cf5bdb13910bc62
Michael Fenwick <mickyfen17@aol.com>
2017-05-01 14:54:23 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(app.get('port'), () => {
+  console.log(`Server is listening on ${app.get('port')}.`)
+})
