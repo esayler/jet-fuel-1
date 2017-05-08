0e1d9bc1d1d9198c5f36c29db10ceff90e02ebb4
Eric Sayler <easayler@gmail.com>
2017-05-08 09:27:38 -0600

    use morgan only in dev, fix test script

--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const router = require('./router');
 const redirect = require('./redirect');
 const favicon = require('serve-favicon');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+}
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(favicon(path.join(__dirname, '../', 'public', 'favicon.ico')));
